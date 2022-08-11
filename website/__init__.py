6cc2a213d53c7b8aa337e0980dbc735b013ace0e
aryan <aryanp862002@gmail.com>
2022-08-11 19:54:43 -0400

    production changes

--- website/__init__.py
+++ website/__init__.py
@@ -15,15 +15,15 @@ db =SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     #app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') #or'sqlite:///mainDatabase.db'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mainDatabase.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     cloudinary.config(
-        cloud_name=os.environ.get('CLOUD_NAME'),
-        api_key=os.environ.get('API_KEY'),
-        api_secret=os.environ.get('API_SECRET')
+        cloud_name=os.getenv('CLOUD_NAME'),
+        api_key=os.getenv('API_KEY'),
+        api_secret=os.getenv('API_SECRET')
     )
 
 
