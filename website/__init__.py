920faa81e7052e7b80c66518a7dfbeebaaa2b167
aryan <aryanp862002@gmail.com>
2022-08-11 18:58:05 -0400

    initial commit

--- website/__init__.py
+++ website/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app = Flask(__name__)
     #app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or'sqlite:///mainDatabase.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') #or'sqlite:///mainDatabase.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
